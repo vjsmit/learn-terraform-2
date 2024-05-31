930985bc89d89bae9d1a7c53135caec0d4406528
smvijay <vj.smit97@gmail.com>
2024-05-31 02:51:43 -0400

    tf output

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -1,19 +1,57 @@
-variable "sample" {
-  default = 100
+#variable "sample" {
+#  default = 100
+#}
+#
+#variable "sample1" {
+#  default = "Hello World"
+#}
+#
+#output "sam" {
+#  value = var.sample
+#}
+#
+#output "sam1" {
+#  value = var.sample1
+#}
+#
+#output "sam2" {
+#  value = "value of sample1 is ${var.sample1}"
+#}
+
+variable "course" {
+  default = "DevOps"
 }
 
-variable "sample1" {
-  default = "Hello World"
+variable "courses" {
+  default = [
+  "DevOps321",
+  "AWS",
+  "Python"]
 }
 
-output "sam" {
-  value = var.sample
+variable "courses_details" {
+  default = {
+    DevOps = {
+      name = "DevOps"
+      timing = "6 AM"
+      duration = 90 mins
+    }
+    AWS = {
+      name = "AWS"
+      timing = "8 AM"
+      duration = 60 mins
+    }
+  }
 }
 
-output "sam1" {
-  value = var.sample1
+output "course" {
+  value = "Variable type is plain ${var.course}"
 }
 
-output "sam2" {
-  value = "value of sample1 is ${var.sample1}"
+output "courses" {
+  value = "Variable type is list ${var.courses}"
+}
+
+output "courses_details" {
+  value = "Variable type is map ${var.courses_details}"
 }
