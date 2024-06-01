45ce3ca8046369e3bc054edab7f9129a6a3a9547
smvijay <vj.smit97@gmail.com>
2024-06-01 11:01:20 -0400

    tf output

--- 09-loops/count/main.tf
+++ 09-loops/count/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "web" {
-  count = 3
+  count = length(var.instances)
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloWorld-${count.index}"
   }
 }
 
