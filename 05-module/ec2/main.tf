d41f8c4c75b59d7d879c04effb5a03154323c290
smvijay <vj.smit97@gmail.com>
2024-06-01 03:14:01 -0400

    tf output

--- 05-module/ec2/main.tf
+++ 05-module/ec2/main.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.sg.id
-  cidr_ipv4         = ["0.0.0.0/0"]
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
