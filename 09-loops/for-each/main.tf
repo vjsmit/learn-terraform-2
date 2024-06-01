72fe756a3998ac1268fd2dee1150739da22aae64
smvijay <vj.smit97@gmail.com>
2024-06-01 11:32:06 -0400

    tf output

---
+++ 09-loops/for-each/main.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "web" {
+  count = length(var.instances)
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    #Name = var.instances[count.index]
+    Name = element(var.instances, count.index)
+  }
+}
+
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+variable "instances" {
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.small"
+    }
+    mongodb = {
+      name = "mongodb"
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.nano"
+    }
+  }
+}
