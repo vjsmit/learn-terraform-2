d7b18c5ca3f6351dc114aadb704f0c414d542a0d
smvijay <vj.smit97@gmail.com>
2024-06-02 02:30:47 -0400

    tf output

--- roboshop-app/test.tf
+++ roboshop-app/test.tf
@@ -1,22 +1,24 @@
-module "instances" {
-  for_each = var.instances
+module "frontend" {
   source = "./ec2"
-  name = each.key
+  name = "frontend"
 }
-
-variable "instances" {
-  default = {
-    frontend = {}
-    mongodb = {}
-    catalogue = {}
-    redis = {}
-    user = {}
-    cart = {}
-    mysql = {}
-    shipping = {}
-    rabbitmq = {}
-    payment = {}
-    dispatch = {}
-  }
+module "mongodb" {
+  source = "./ec2"
+  name = "mongodb"
 }
-
+module "catalogue" {
+  source = "./ec2"
+  name = "catalogue"
+}
+module "redis" {
+  source = "./ec2"
+  name = "redis"
+}
+module "user" {
+  source = "./ec2"
+  name = "user"
+}
+module "cart" {
+  source = "./ec2"
+  name = "cart"
+}
