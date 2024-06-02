0cd533f03dff01e67a66f9f8b0096af35dac3e55
smvijay <vj.smit97@gmail.com>
2024-06-01 22:26:43 -0400

    tf output

--- 05-module/ec2/main.tf
+++ 05-module/ec2/main.tf
@@ -24,9 +24,9 @@ resource "aws_security_group" "sg" {
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 0
+  from_port         = 22
   ip_protocol       = "tcp"
-  to_port           = 0
+  to_port           = 22
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
--- 07-provisioner/main.tf
+++ 07-provisioner/main.tf
@@ -2,11 +2,13 @@ resource "aws_instance" "sample" {
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-06455962b48979e39"]
+
   tags = {
     Name = "sample"
   }
 
   provisioner "remote-exec" {
+
     connection {
       type     = "ssh"
       user     = "centos"
@@ -16,7 +18,9 @@ resource "aws_instance" "sample" {
 
     inline = [
       "sudo labauto ansible",
-      "ansible-pull - i localhost, -U https://github.com/vjsmit/roboshop-ansible-1 roboshop.yml -e env=dev -e role_name=frontend",
+      "ansible-pull -i localhost, -U https://github.com/vjsmit/roboshop-ansible-3 main.yml -e env=dev -e role_name=frontend"
+#      "ansible-pull - i localhost, -U https://github.com/vjsmit/roboshop-ansible-3 main.yml -e env=dev -e role_name=frontend"
+
     ]
   }
 }
