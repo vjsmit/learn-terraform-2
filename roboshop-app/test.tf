5b421f44de24a04df70095a9270595f23e4c901c
smvijay <vj.smit97@gmail.com>
2024-06-02 03:03:02 -0400

    tf output

--- roboshop-app/test.tf
+++ roboshop-app/test.tf
@@ -2,23 +2,23 @@ module "frontend" {
   source = "./ec2"
   name = "frontend"
 }
-module "mongodb" {
-  source = "./ec2"
-  name = "mongodb"
-}
-module "catalogue" {
-  source = "./ec2"
-  name = "catalogue"
-}
-module "redis" {
-  source = "./ec2"
-  name = "redis"
-}
-module "user" {
-  source = "./ec2"
-  name = "user"
-}
-module "cart" {
-  source = "./ec2"
-  name = "cart"
-}
+#module "mongodb" {
+#  source = "./ec2"
+#  name = "mongodb"
+#}
+#module "catalogue" {
+#  source = "./ec2"
+#  name = "catalogue"
+#}
+#module "redis" {
+#  source = "./ec2"
+#  name = "redis"
+#}
+#module "user" {
+#  source = "./ec2"
+#  name = "user"
+#}
+#module "cart" {
+#  source = "./ec2"
+#  name = "cart"
+#}
